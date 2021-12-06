bef06c3d215b5f1137439765704eb2496c07ca5b
Peter Burns <petercb@users.noreply.github.com>
2021-12-05 20:24:15 -0500

    Add minecraft-java build
    * Add java optimizations to BedrockConnect container
    * Update minecraft-java container to use Paper 1.17.1
    * Add ViaVersion plugin for 1.18 client support
    * Add BetterSleep4 plugin

---
+++ minecraft-java/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+if [ -z "${JAVA_OPTS}" ] ; then
+    JAVA_OPTS="-XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:+ParallelRefProcEnabled -XX:+AlwaysPreTouch -XX:MaxInlineLevel=15"
+fi
+
+mkdir -p plugins
+for plugin in /plugins/*.jar ; do
+    ln -sf "${plugin}" plugins/
+done
+
+java "${JAVA_OPTS}" -jar "${JAR_FILE:-/minecraft.jar}" "$@"
