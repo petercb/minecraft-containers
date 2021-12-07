93ed585dae9548c7b1e53221d1d7d678d91126aa
Peter Burns <petercb@users.noreply.github.com>
2021-12-06 21:54:25 -0500

    Add velocity-proxy build, use buildx for all builds

---
+++ velocity-proxy/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+echo "Running in $(pwd)"
+
+echo "Initializing plugins"
+mkdir -p plugins
+for plugin in /plugins/*.jar ; do
+    ln -sfv "${plugin}" plugins/
+done
+
+command="java"
+if [ -z "${JAVA_OPTS}" ] ; then
+    command="${command} -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:+ParallelRefProcEnabled -XX:+AlwaysPreTouch -XX:MaxInlineLevel=15"
+else
+    command="${command} ${JAVA_OPTS}"
+fi
+command="${command} -jar ${JAR_FILE:-/velocity.jar} $*"
+
+echo "Running: '${command}'"
+exec ${command}
