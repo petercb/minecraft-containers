c56cc6367e219579c58187c8ad1c4606f0dd4062
Peter Burns <petercb@users.noreply.github.com>
2021-12-11 12:15:10 -0500

    Use newer docker engine

--- local-build-test.sh
+++ local-build-test.sh
@@ -3,10 +3,19 @@
 set -e
 set -u
 
+function cleanup {
+    echo "Stopping builder"
+    docker buildx stop container
+}
+
+trap cleanup EXIT
+
+docker buildx create --driver docker-container --name container --node container0 --use
+
 for image in bedrock-connect minecraft-java velocity-proxy
 do
     pushd "${image}"
-    docker build -t petercb/${image} .
-    container-structure-test test --config container-structure-test.yaml --image petercb/${image}
+    docker buildx build --load --cache-from petercb/${image}:cache -t petercb/${image}:latest .
+    container-structure-test test --config container-structure-test.yaml --image petercb/${image}:latest
     popd
 done
