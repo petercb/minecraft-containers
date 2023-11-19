ab63249b2a4eb13dabf4ab0e8a64a70177b5271c
Peter Burns <petercb@users.noreply.github.com>
2023-11-18 21:16:50 -0500

    Update all containers to latest
    * Bedrock-Connect to 1.39
    * Minecraft (Bedrock) to 1.20.41.02
    * Minecraft (Java/PaperMC) to 1.20.2-291
    * Velocity to build 294
    * Moved ViaVersion to Velocity container

---
+++ README.md
@@ -0,0 +1,3 @@
+# Minecraft Containers
+
+[![CircleCI](https://dl.circleci.com/status-badge/img/gh/petercb/minecraft-containers/tree/master.svg?style=svg)](https://dl.circleci.com/status-badge/redirect/gh/petercb/minecraft-containers/tree/master)
--- local-build-test.sh
+++ local-build-test.sh
@@ -12,8 +12,9 @@ trap cleanup EXIT
 
 docker buildx create --driver docker-container --name container --node container0 --use
 
-for image in bedrock-connect minecraft-java velocity-proxy
+for dockerfile in ${1:-*}/Dockerfile
 do
+    image="$(dirname ${dockerfile})"
     pushd "${image}"
     docker buildx build --builder container --load --cache-from petercb/${image}:cache -t petercb/${image}:latest .
     container-structure-test test --config container-structure-test.yaml --image petercb/${image}:latest
