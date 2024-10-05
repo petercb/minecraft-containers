e484e3cb353bfbe718d89d655e0871cf3d966afc
Peter Burns <petercb@users.noreply.github.com>
2024-10-05 12:42:09 -0400

    velocity-proxy: Update to 3.3.0-436
    Set JAVA_OPTS defaults in container env, simplifying entrypoint script

--- velocity-proxy/entrypoint.sh
+++ velocity-proxy/entrypoint.sh
@@ -2,14 +2,7 @@
 
 echo "Running in $(pwd)"
 
-command="java"
-if [ -z "${JAVA_OPTS}" ] ; then
-    command="${command} -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:+UnlockExperimentalVMOptions -XX:+ParallelRefProcEnabled \
-        -XX:+AlwaysPreTouch -XX:MaxInlineLevel=15"
-else
-    command="${command} ${JAVA_OPTS}"
-fi
-command="${command} -jar ${JAR_FILE:-/velocity.jar} $*"
+command="java ${JAVA_OPTS:-} -jar ${JAR_FILE:-/velocity.jar} $*"
 
 echo "Running: '${command}'"
 exec ${command}
